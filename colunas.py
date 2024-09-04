68d6c57366c3f018b2f677fff17b072018c8bb17
marco <marcosf1625@gmail.com>
2024-09-04 17:07:58 -0300

    feat: colunas para base

---
+++ colunas.py
@@ -0,0 +1,28 @@
+loja_columns = [
+    'gerente',
+    'supervisor',
+    'agencia',
+    'rede',
+    'endereco',
+    'cidade',
+    'uf',
+    'cod_loja_gm'
+]
+
+produto_columns = [
+    'haste_flexivel_75',
+    'haste_flexivel_300_und',
+    'algodao_bola_50gr',
+    'algodao_rolo_25gr',
+    'algodao_disco_50_und',
+    'toalha_umed_50_und',
+    'toalha_umed_100_und',
+    'sabonete_baby_400ml',
+    'shampoo_200ml',
+    'absorvente_abas_8_und',
+    'absorvente_abas_16_und',
+    'absorvente_noturno_8_und',
+    'protetor_diario_15_und',
+    'protetor_diario_40_und',
+    'sabonete_intimo'
+]
